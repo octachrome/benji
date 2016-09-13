32aac9f8b389348b950f9efff7f566a1b25323a0
Chris Brown <chris@thebrown.net>
2016-09-13 21:03:49 +0100

    Ability to build a static site

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,56 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         copy: {
-            lib: {
+            static: {
+                files: [
+                    {
+                        'static/lib/jquery.js': 'node_modules/jquery/dist/jquery.min.js',
+                        'static/lib/knockout.js': 'node_modules/knockout/build/output/knockout-latest.js',
+                        'static/lib/lodash.js': 'node_modules/lodash/lodash.js',
+                        'static/lib/long.js': 'node_modules/long/dist/long.js',
+                        'static/lib/chance.js': 'node_modules/chance/chance.js',
+                        'static/lib/pixi.js': 'node_modules/pixi.js/bin/pixi.min.js',
+                    },
+                    {
+                        expand: true,
+                        dest: 'static/',
+                        src: ['lib/**', 'js/**']
+                    },
+                    {
+                        expand: true,
+                        cwd: 'www',
+                        dest: 'static/',
+                        src: '**'
+                    }
+                ]
+            },
+            static_ms: {
+                options: {
+                    process: function (content) {
+                        return content.replace(/module\.exports/g, 'window.ms');
+                    }
+                },
                 files: {
-                    'lib/jquery.js': 'node_modules/jquery/dist/jquery.min.js',
-                    'lib/knockout.js': 'node_modules/knockout/build/output/knockout-latest.js',
-                    'lib/lodash.js': 'node_modules/lodash/lodash.js',
-                    'lib/long.js': 'node_modules/long/dist/long.js',
-                    'lib/chance.js': 'node_modules/chance/chance.js',
-                    'lib/pixi.js': 'node_modules/pixi.js/bin/pixi.min.js'
+                    'static/lib/ms.js': 'node_modules/ms/index.js'
                 }
+            },
+            data: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '../benji-data',
+                        dest: 'static/',
+                        src: 'anim/**'
+                    },
+                    {
+                        'static/script.benji': '../benji-data/scripts/test.benji'
+                    }
+                ]
             }
         }
     });
 
-    grunt.registerTask('default', 'copy');
+    grunt.registerTask('static', ['copy:static', 'copy:static_ms']);
+    grunt.registerTask('site', ['static', 'copy:data']);
+    grunt.registerTask('default', 'static');
 };
--- server.js
+++ server.js
@@ -17,20 +17,8 @@ function _startServer(_animPath, _scriptPath) {
   animRoute = express.static(_animPath);
   if (!server) {
     app = express();
-    app.use('/', express.static(__dirname + '/www'));
-    app.use('/js', express.static(__dirname + '/js'));
-    app.use('/lib', express.static(__dirname + '/lib'));
-    app.get('/lib/ms.js', function (req, res, next) {
-      fs.readFile(require.resolve('ms/index.js'), {encoding: 'utf8'}, function (err, data) {
-        if (err) {
-          next(err);
-        }
-        else {
-          res.set('Content-Type', 'text/javascript');
-          res.send(data.replace(/module\.exports/g, 'window.ms'));
-        }
-      });
-    });
+    app.use('/', express.static(__dirname + '/static'));
+    app.use('/preview.html', express.static(__dirname + '/static/www/index.html'));
     app.use('/anim', function (req, res, next) {
       animRoute(req, res, next);
     });
